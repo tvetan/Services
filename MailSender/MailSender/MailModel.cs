d48064ec62dd7ecfee7e952e66dc9e6d6af086e2
tvetan <tvetan@gbg.bg>
2013-08-22 13:48:00 +0300

    Adding Mail Sender

---
+++ MailSender/MailSender/MailModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace MailSender
+{
+    public class MailModel
+    {
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+    }
+}
---
+++ MailSender/MailSender/MailSenderMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Typesafe.Mailgun;
+
+namespace MailSender
+{
+    public class MailSenderMain
+    {
+        static void Main(string[] args)
+        {
+            MailModel mail = new MailModel();
+
+            Console.Write("From: ");
+            mail.From = Console.ReadLine();
+
+            Console.Write("To: ");
+            mail.To = Console.ReadLine();
+
+            Console.Write("Subject: ");
+            mail.Subject = Console.ReadLine();
+
+            Console.Write("Body: ");
+            mail.Body = Console.ReadLine();
+
+            var mailClient = new MailgunClient(MailSenderSettings.Default.Domain, MailSenderSettings.Default.ApiKey);
+            mailClient.SendMail(new System.Net.Mail.MailMessage(mail.From, mail.To)
+            {
+                Subject = mail.Subject,
+                Body = mail.Body
+            });
+        }
+    }
+}
